bcf45072042597a53b1fe3153ee7b25156045109
Adebola <onedebos@gmail.com>
2020-03-18 23:55:49 +0100

    add dev.to link to profiles

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -33,17 +33,19 @@ export default function Left({ handleDarkMode, clicked }) {
         </p>
       </div>
       <div className="profiles">
-        <p className="description">
-          <a href="https://github.com/onedebos/">github</a>&nbsp;| &nbsp;
-          <a href="mailto: adebola.rb.js@gmail.com">email</a>&nbsp;|&nbsp;
+        <p className="description profiles">
+          <a href="https://github.com/onedebos/">github</a>&nbsp; &nbsp;
+          <a href="mailto: adebola.rb.js@gmail.com">email</a>&nbsp;&nbsp;
           <a href="https://www.linkedin.com/in/adebola-niran">linkedin</a>
-          &nbsp;| &nbsp;<a href="https://twitter.com/debosthefirst">twitter</a>
-          &nbsp;|&nbsp;
-          <a href="https://medium.com/@adebola.niran">medium</a> &nbsp;| &nbsp;
+          &nbsp; &nbsp;<a href="https://twitter.com/debosthefirst">twitter</a>
+          &nbsp;&nbsp;
+          <a href="https://medium.com/@adebola.niran">medium</a> &nbsp; &nbsp;
           <a href="https://gist.github.com/onedebos/08c6d089a55e53e07b9bf56451c912fc#file-adebola-cv-md">
             resume
           </a>
-          &nbsp; |&nbsp;<a href="https://linktr.ee/adebol_a">linktree</a>{" "}
+          &nbsp;&nbsp;
+          <a href="https://linktr.ee/adebol_a">linktree</a> &nbsp;&nbsp;
+          <a href="https://dev.to/@debosthefirst">dev.to</a>{" "}
         </p>
       </div>
     </div>
