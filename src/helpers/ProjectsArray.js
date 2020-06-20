8a01618f6a437dee3b7ed9fc020abf70d069acf5
Adebola <onedebos@gmail.com>
2020-06-20 22:48:24 +0100

    update projects

--- src/helpers/ProjectsArray.js
+++ src/helpers/ProjectsArray.js
@@ -8,27 +8,17 @@ const projects = [
       "https://res.cloudinary.com/onedebeos/image/upload/f_auto,q_auto/v1582185345/personal-portfolio/app_screenshot_mvkavi.png",
     ghLink: "https://github.com/onedebos/mentaLY-rails-API"
   },
-
   {
     id: 2,
-    projectName: "CoronaQL",
-    projectBuiltWith: "Built with Rails/GraphQL",
-    projectUrl: "https://corona-ql.herokuapp.com/graphiql",
+    projectName: "Prewedding",
+    projectBuiltWith: "GatsbyJS and GraphQL",
+    projectUrl: "https://olamidefemi.com/",
     imgUrl:
-      "https://res.cloudinary.com/onedebeos/image/upload/f_auto,q_auto:best/v1585173273/personal-portfolio/Screen_Shot_2020-03-25_at_22.54.01_ii4yfw.png",
-    ghLink: "https://github.com/onedebos/corona-ql"
+      "https://res.cloudinary.com/onedebeos/image/upload/c_scale,h_1536/v1592689434/personal-portfolio/Screen_Shot_2020-06-20_at_22.43.22_dt8y1a.png",
+    ghLink: "https://github.com/onedebos/FandO"
   },
   {
     id: 3,
-    projectName: "Lrn::Fast",
-    projectBuiltWith: "with React and Redux toolkit",
-    projectUrl: "https://lrn-fast.netlify.com/",
-    imgUrl:
-      "https://res.cloudinary.com/onedebeos/image/upload/f_auto,q_auto/v1586895384/personal-portfolio/Screen_Shot_2020-04-14_at_21.15.22_h3ckk8.png",
-    ghLink: "https://github.com/onedebos/learn-fast"
-  },
-  {
-    id: 8,
     projectName: "Foodies",
     projectBuiltWith: "Built with Rails and React",
     projectUrl: "https://foodies-stop.netlify.app/",
@@ -37,7 +27,27 @@ const projects = [
     ghLink: "https://github.com/Tripple-A/Chef-React-App"
   },
   {
-    id: 7,
+    id: 4,
+    projectName: "Lrn::Fast",
+    projectBuiltWith: "with React and Redux toolkit",
+    projectUrl: "https://lrn-fast.netlify.com/",
+    imgUrl:
+      "https://res.cloudinary.com/onedebeos/image/upload/f_auto,q_auto/v1586895384/personal-portfolio/Screen_Shot_2020-04-14_at_21.15.22_h3ckk8.png",
+    ghLink: "https://github.com/onedebos/learn-fast"
+  },
+
+  {
+    id: 5,
+    projectName: "CoronaQL",
+    projectBuiltWith: "Built with Rails/GraphQL",
+    projectUrl: "https://corona-ql.herokuapp.com/graphiql",
+    imgUrl:
+      "https://res.cloudinary.com/onedebeos/image/upload/f_auto,q_auto:best/v1585173273/personal-portfolio/Screen_Shot_2020-03-25_at_22.54.01_ii4yfw.png",
+    ghLink: "https://github.com/onedebos/corona-ql"
+  },
+
+  {
+    id: 6,
     projectName: "OwoNiKoko",
     projectBuiltWith: "with Rails/React/Context API",
     projectUrl: "https://kind-edison-586550.netlify.com/",
@@ -46,7 +56,7 @@ const projects = [
     ghLink: "https://github.com/onedebos/owonikoko"
   },
   {
-    id: 5,
+    id: 7,
     projectName: "MicroFacebook",
     projectBuiltWith: "with Rails + fb Auth",
     projectUrl: "https://micro-social.herokuapp.com/",
@@ -55,23 +65,13 @@ const projects = [
     ghLink: "https://github.com/onedebos/micro-facebook"
   },
   {
-    id: 6,
+    id: 8,
     projectName: "Tic-Tac-Toe",
     projectBuiltWith: "Built with Javascript",
     projectUrl: "https://zen-beaver-6debcc.netlify.com",
     imgUrl:
       "https://res.cloudinary.com/onedebeos/image/upload/f_auto,q_auto/v1582245511/personal-portfolio/Screen_Shot_2020-02-21_at_01.37.35_lqntqz.png",
     ghLink: "https://github.com/onedebos/js-tictactoe"
-  },
-
-  {
-    id: 8,
-    projectName: "Weatherify",
-    projectBuiltWith: "with Fetch and Webpack",
-    projectUrl: "https://kind-carson-bb3068.netlify.com/",
-    imgUrl:
-      "https://res.cloudinary.com/onedebeos/image/upload/f_auto,q_auto:best/v1585167888/personal-portfolio/Screenshot_2020-03-25_Weatherify_-_live_weather_data_1_toqqit.png",
-    ghLink: "https://github.com/onedebos/weather-app"
   }
 ];
 
