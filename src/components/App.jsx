757a64ed2d6ad42638e8edf09693acbcf1e8b1ea
Adebola <onedebos@gmail.com>
2020-07-08 13:23:13 +0100

    fix errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Menu from "./Menu";
 import Home from "./Home";
 import Pro from "./Pro";
@@ -9,23 +9,23 @@ import projects from "../helpers/ProjectsArray";
 import WorkWithMe from "./WorkWithMe";
 
 export default function App() {
-  const [clicked, setClicked] = useState({ mode: false });
-  const handleDarkMode = () => {
-    setClicked((prevState) => ({
-      ...prevState,
-      mode: !clicked.mode,
-    }));
+  // const [clicked, setClicked] = useState({ mode: false });
+  // const handleDarkMode = () => {
+  //   setClicked((prevState) => ({
+  //     ...prevState,
+  //     mode: !clicked.mode,
+  //   }));
 
-    const container = document.querySelector(".container");
-    const wrapperRight = document.querySelector(".wrapper-right");
-    const mobileFooter = document.querySelector(".mobileFooter");
-    const darkModeBtn = document.querySelector(".darkModeBtn");
+  //   const container = document.querySelector(".container");
+  //   const wrapperRight = document.querySelector(".wrapper-right");
+  //   const mobileFooter = document.querySelector(".mobileFooter");
+  //   const darkModeBtn = document.querySelector(".darkModeBtn");
 
-    wrapperRight.classList.toggle("dark-mode");
-    container.classList.toggle("dark-mode");
-    mobileFooter.classList.toggle("dark-mode");
-    darkModeBtn.classList.toggle("dark-mode");
-  };
+  //   wrapperRight.classList.toggle("dark-mode");
+  //   container.classList.toggle("dark-mode");
+  //   mobileFooter.classList.toggle("dark-mode");
+  //   darkModeBtn.classList.toggle("dark-mode");
+  // };
   return (
     <div>
       <Menu />
