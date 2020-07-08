55e420b414eb5991b191a29a5b9665c597c4398a
Adebola <onedebos@gmail.com>
2020-07-08 14:12:15 +0100

    increase size of some of projects

--- src/components/Pro.jsx
+++ src/components/Pro.jsx
@@ -4,7 +4,7 @@ import Project from "./Project";
 const Pro = ({ projects }) => {
   return (
     <div className="py-10 ">
-      <div className="font-bold text-3xl mb-8 md:mb-20 text-center">
+      <div className="font-bold text-3xl md:text-6xl mb-8 md:mb-20 text-center">
         <mark>Some of my Projects.</mark>
       </div>
       {projects.map((project) => {
