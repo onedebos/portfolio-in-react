f17323f1dceb27fd05f7dd835f222c60f74f5159
Adebola <onedebos@gmail.com>
2020-07-08 20:34:27 +0100

    fix read my work

--- src/components/WorkWithMe.js
+++ src/components/WorkWithMe.js
@@ -60,7 +60,7 @@ const WorkWithMe = () => {
           className="text-center font-bold text-4xl md:text-6xl"
           data-sal="slide-right"
         >
-          <mark>Read my articles.</mark>
+          <mark>Read my work.</mark>
         </h1>
         <p className="text-center text-2xl" data-sal="slide-down">
           <a
