c50187c6271cbcdf48b48da4dfdfad381ac28638
Adebola <onedebos@gmail.com>
2020-07-27 15:37:11 +0100

    optimize tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,14 @@
-module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
+const dev = {
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
+
+if (process.env.NODE_ENV === "production") {
+  dev.plugins.push(
     require("@fullhuman/postcss-purgecss")({
       content: ["./src/components/**.jsx", "./src/components/**.js"],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    }),
-  ],
-};
+    })
+  );
+}
+
+module.exports = dev;
