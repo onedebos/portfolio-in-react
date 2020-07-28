6158c78b416c1ab52ef9435dd5eb0309d50c9fd6
Adebola <onedebos@gmail.com>
2020-07-28 17:03:31 +0100

    add og meta tags

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,8 @@ import sal from "sal.js";
 import "../../node_modules/sal.js/dist/sal.css";
 import projects from "../helpers/ProjectsArray";
 import WorkWithMe from "./WorkWithMe";
+import { DESCRIPTION, TITLE, TYPE, URL } from "../helpers/meta";
+import Debs from "../helpers/debs-og.jpg";
 
 ReactGA.initialize("UA-171680853-2");
 ReactGA.pageview(window.location.pathname + window.location.search);
@@ -24,6 +26,20 @@ export default function App() {
         <meta charSet="utf-8" />
         <title>Adebola | Fullstack Engineer</title>
         <link rel="adebola adeniran" href="https://adebola.dev" />
+        <meta name="og:url" content={URL} />
+        <meta name="og:title" content={TITLE} />
+        <meta name="og:description" content={DESCRIPTION} />
+        <meta name="og:type" content={TYPE} />
+        <meta name="og:image" content={Debs} />
+
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:image" content={Debs} />
+        <meta
+          name="twitter:card"
+          content="A dev passionate about Jollof rice and plantain!"
+        />
+
         <meta
           name="Adebola Adeniran - Fullstack Engineer, Software developer"
           content="Adebola Adeniran - Fullstack Engineer"
