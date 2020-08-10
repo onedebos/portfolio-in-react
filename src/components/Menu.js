d155c84626bc9ba42600c895f70edfb18a7d6814
Adebola <onedebos@gmail.com>
2020-08-10 10:13:17 +0100

    fix mobile menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ScrollIntoView from "react-scroll-into-view";
+import "./Menu.css";
 
 const Menu = ({ ga }) => {
   const handleClick = () => {
@@ -15,30 +16,93 @@ const Menu = ({ ga }) => {
       action: "To blog",
     });
   };
+
+  const navToggle = () => {
+    var btn = document.getElementById("menuBtn");
+    var nav = document.getElementById("menu");
+
+    btn.classList.toggle("open");
+    nav.classList.toggle("flex");
+    nav.classList.toggle("hidden");
+  };
   return (
-    <div className="bg-white p-1 py-3">
-      <div className="flex justify-around px-10">
-        <div className="hidden md:block text-4xl font-bold text-yellow-600">
-          AA
-        </div>
-        <div className="flex justify-between">
-          <a
-            href="https://blog.adebola.dev"
-            className="font-bold mt-2 border border-2 rounded-sm border-black px-6 py-3 text-sm button text mr-2"
-            onClick={trackBlog}
-          >
-            READ BLOG
-          </a>
-          <ScrollIntoView selector="#projects">
+    // <div className="bg-white p-1 py-3">
+    //   <div className="flex justify-around px-10">
+    //     <div className="hidden md:block text-4xl font-bold text-yellow-600">
+    //       AA
+    //     </div>
+    //     <div className="flex justify-between">
+    //       <a
+    //         href="https://blog.adebola.dev"
+    //         className="font-bold mt-2 border border-2 rounded-sm border-black px-6 py-3 text-sm button text mr-2"
+    //         onClick={trackBlog}
+    //       >
+    //         READ BLOG
+    //       </a>
+    //       <ScrollIntoView selector="#projects">
+    //         <button
+    //           className="font-bold mt-2 border border-2 rounded-sm border-black px-6 py-3 text-sm button text"
+    //           onClick={handleClick}
+    //         >
+    //           SEE PROJECTS
+    //         </button>
+    //       </ScrollIntoView>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="bg-white md:py-4">
+      <header
+        id="top"
+        className="w-full flex flex-col fixed sm:relative pin-t pin-r pin-l lg:max-w-screen-lg m-auto"
+      >
+        <nav
+          id="site-menu"
+          className="flex flex-col sm:flex-row w-full justify-between items-center px-4 sm:px-6 py-1 shadow sm:shadow-none bg-gray-200 md:bg-white"
+        >
+          <div className="w-full sm:w-auto self-start sm:self-center flex flex-row sm:flex-none flex-no-wrap justify-between items-center">
+            <a href="/" className="py-1">
+              <h1 className="font-bold text-4xl text-yellow-600">AA</h1>
+            </a>
+
             <button
-              className="font-bold mt-2 border border-2 rounded-sm border-black px-6 py-3 text-sm button text"
-              onClick={handleClick}
+              id="menuBtn"
+              className="hamburger block sm:hidden focus:outline-none"
+              type="button"
+              onClick={navToggle}
             >
-              SEE PROJECTS
+              <span className="hamburger__top-bun"></span>
+              <span className="hamburger__bottom-bun"></span>
             </button>
-          </ScrollIntoView>
-        </div>
-      </div>
+          </div>
+          <div
+            id="menu"
+            className="w-full sm:w-auto self-end sm:self-center sm:flex flex-col sm:flex-row items-center h-full py-1 pb-4 sm:py-0 sm:pb-0 hidden"
+          >
+            <a
+              onClick={trackBlog}
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://blog.adebola.dev"
+              className="text-lg w-full no-underline sm:w-auto sm:pr-4 py-2 sm:py-1 sm:pt-2 md:border md:rounded-sm md:border-black md:px-6 button text"
+            >
+              READ BLOG
+            </a>
+
+            <ScrollIntoView
+              selector="#projects"
+              className="w-full mt-2 md:w-auto md:mt-0"
+            >
+              <button
+                onClick={handleClick}
+                className="text-dark text-lg no-underline sm:w-auto sm:px-4 py-2 sm:py-1 sm:pt-2 md:border md:rounded-sm border-black md:px-6 button text md:ml-2 focus:outline-none"
+              >
+                SEE PROJECTS
+              </button>
+            </ScrollIntoView>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 };
