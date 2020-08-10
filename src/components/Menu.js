c9e6751046c8a04bac747722ba3c16ed8e7db9a2
Adebola <onedebos@gmail.com>
2020-08-10 10:14:50 +0100

    refactor menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ScrollIntoView from "react-scroll-into-view";
-import "./Menu.css";
 
 const Menu = ({ ga }) => {
   const handleClick = () => {
@@ -26,31 +25,6 @@ const Menu = ({ ga }) => {
     nav.classList.toggle("hidden");
   };
   return (
-    // <div className="bg-white p-1 py-3">
-    //   <div className="flex justify-around px-10">
-    //     <div className="hidden md:block text-4xl font-bold text-yellow-600">
-    //       AA
-    //     </div>
-    //     <div className="flex justify-between">
-    //       <a
-    //         href="https://blog.adebola.dev"
-    //         className="font-bold mt-2 border border-2 rounded-sm border-black px-6 py-3 text-sm button text mr-2"
-    //         onClick={trackBlog}
-    //       >
-    //         READ BLOG
-    //       </a>
-    //       <ScrollIntoView selector="#projects">
-    //         <button
-    //           className="font-bold mt-2 border border-2 rounded-sm border-black px-6 py-3 text-sm button text"
-    //           onClick={handleClick}
-    //         >
-    //           SEE PROJECTS
-    //         </button>
-    //       </ScrollIntoView>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="bg-white md:py-4">
       <header
         id="top"
