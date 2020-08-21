3f30a27c19c7f3bfe9d903adf2123ef5ee72331d
Adebola <onedebos@gmail.com>
2020-08-21 12:41:19 +0100

    add background to projects

--- src/components/Pro.jsx
+++ src/components/Pro.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Project from "./Project";
+import "../styles/tailwind.css";
 
 const Pro = ({ projects, ga, theRef }) => {
   return (
-    <div id="projects" ref={theRef}>
+    <div id="projects" ref={theRef} className="projectsBg">
       <div className="font-bold text-3xl md:text-6xl mb-8 md:mb-20 text-center mt-8 z-10">
         <mark>Some of my Projects.</mark>
       </div>
